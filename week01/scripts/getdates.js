ab53e5d6899717a94dbe3b804a77f50d7e7c7beb
Emeodu-Iheanachor <iheralchi123@gmail.com>
2026-01-12 12:40:12 +0100

    Another update

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,5 +1,2 @@
-// Current Year
 document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Last Modified Date
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
