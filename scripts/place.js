a7995b62d0700aa948e3f4a01bbf2ad38e160d5d
Emeodu-Iheanachor <iheralchi123@gmail.com>
2026-01-24 14:27:23 +0100

    NGERIA UPDATE

---
+++ scripts/place.js
@@ -0,0 +1,23 @@
+// FOOTER DATES
+document.getElementById("current-year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// STATIC WEATHER VALUES
+const temperature = 30; // °C
+const windSpeed = 10;   // km/h
+
+// ONE-LINE WIND CHILL FUNCTION (METRIC)
+function calculateWindChill(temp, speed) {
+  return 13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16);
+}
+
+// APPLY CONDITIONS
+let windChill = "N/A";
+
+if (temperature <= 10 && windSpeed > 4.8) {
+  windChill = calculateWindChill(temperature, windSpeed).toFixed(1) + " °C";
+}
+
+document.getElementById("windchill").textContent = windChill;
+    // Change hamburger to X
+    hamburger.textContent = isOpen ? "☰" : "✖";
